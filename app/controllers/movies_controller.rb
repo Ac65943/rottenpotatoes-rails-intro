7ba36d74c33eeaac5ef021444cfc5c7f760e4428
Ubuntu <ubuntu@ip-172-31-26-100.ec2.internal>
2019-09-26 13:43:16 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,33 +11,13 @@ class MoviesController < ApplicationController
   end
 
   def index
-    sort = params[:sort_by] || session[:sort_by]
     @all_ratings = Movie.all_ratings
-    @selected_ratings = params[:ratings] || {}
-
-    if @selected_ratings == {}
-      @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
+    @checkRatings = params[:ratings] || session[:ratings] || {}
+    if @checkRatings == {}
+      @checkRatings = Hash[@all_ratings.map {|rating| [rating, rating]}] #map array to
     end
-
-    if params[:sort_by] != session[:sort_by] or params[:ratings] != session[:ratings]
-      session[:sort_by] = sort
-      session[:ratings] = @selected_ratings
-      redirect_to :sort_by => sort, :ratings => @selected_ratings and return
-    end
-    @movies = Movie.where(rating: @selected_ratings.keys).order(sort)
-    # @all_ratings = Movie.all_ratings
-    # checkRatings= params[:ratings] || session[:ratings] || {}
-
-    # if checkRatings == {}
-    #   checkRatings =  Hash[@all_ratings.map {|rating| [rating, rating]}]
-    # end
-
-    # if params[:sort_by] != session[:sort_by] or params[:ratings] != session[:ratings]
-    #   session[:sort_by] = params[:sort_by] || session[:sort_by]
-    #   session[:ratings] = checkRatings
-    #   redirect_to :sort_by => session[:sort_by], :ratings => checkRatings and return
-    # end
-    # @movies = Movie.where(rating: checkRatings.keys).order(session[:sort_by])
+    sort_by = params[:sort_by] || session[:sort_by]
+    @movies = Movie.where(rating: @checkRatings.keys).order(sort_by)
   end
 
   def new
